13b75355824e00a9d222b06abef03ede4e565f1e
Matthieu Michon <matthieumichon@MacBook-Pro-de-Matthieu.local>
2020-08-06 19:23:55 +0200

    ajout des requetes place

--- routes/index.js
+++ routes/index.js
@@ -58,15 +58,15 @@ router.post('/sportlist',async function(req, res, next) {
 
 
   //Un point WGS84 et une distance en mètres pour le géopositionnement
- /* let latitude = 48.866667
+  let latitude = 48.866667
   let longitude = 2.333333
   let distance = 10000
-*/
 
 
-  let latitude = req.body.lat
-  let longitude = req.body.long
-  let distance = 1000
+
+  //let latitude = req.body.lat
+  //let longitude = req.body.long
+  //let distance = 1000
 
 
 
@@ -124,7 +124,7 @@ res.json({result,uniqList});
 
 
 // Rechgerche par sport
-router.get('/sport',async function(req, res, next) {
+router.post('/sport',async function(req, res, next) {
 
   //Un point WGS84 et une distance en mètres pour le géopositionnement
   let latitude = 48.866667
@@ -166,7 +166,7 @@ router.get('/adressesList',async function(req, res, next) {
 
 
 //recherche des adresses via lat & long
-router.post('/adressesList',async function(req, res, next) {
+router.post('/adressesListCoord',async function(req, res, next) {
 
   //Un point WGS84 et une distance en mètres pour le géopositionnement
   let lon = 2.37
@@ -184,6 +184,86 @@ router.post('/adressesList',async function(req, res, next) {
   res.json({result});
 });
 
+
+//recuperation des points map
+router.post('/listpoint',async function(req, res, next) {
+
+  //Un point WGS84 et une distance en mètres pour le géopositionnement
+  let latitude = 48.866667
+  let longitude = 2.333333
+  let distance = 1000000
+
+  // Liste des activités hors licence etc ...
+
+  var list = request('GET', `https://data.iledefrance.fr/api/records/1.0/search/?dataset=recensement-des-equipements-sportifs&q=&facet=actlib&rows=100&facet=naturelibelle&facet=utilisation&facet=utilisateurs&facet=famille&geofilter.distance=${latitude}%2C${longitude}%2C${distance}`)
+
+
+  var response = JSON.parse(list.getBody())
+
+  var result = response.records
+
+
+  console.log(result)
+  res.json({result});
+});
+
+
+
+
+//recuperation des informations de google place
+router.post('/pointinformation',async function(req, res, next) {
+
+  //Un point WGS84 et une distance en mètres pour le géopositionnement
+  let latitude = 48.86701
+  let longitude = 2.35399
+  let distance = 1000000
+  let name ="petanque"
+
+/*
+
+https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyCXI24AWr0Cv2AXnbh29nVA9Ge7SPIvYBo
+  &key=AIzaSyCXI24AWr0Cv2AXnbh29nVA9Ge7SPIvYBo
+
+
+                      "actnivlib": "Loisir - Entretien - Remise en forme",
+                    "equipementtypelib": "Terrain de pétanque",
+                    "dist": "1511.44144296",
+                    "insnovoie": "93 BIS",
+                    "utilisation": "Récréation sportive",
+                    "insarrondissement": 75103,
+                    "actcode": 6001,
+                    "famille": "Boulodrome",
+                    "naturelibelle": "Découvert",
+                    "equnbequidentique": 1,
+                    "deplib": "Paris",
+                    "insnom": "Square Emile Chautemps",
+                    "depcode": 75,
+                    "gps": [
+                        48.86701,
+                        2.35399
+                    ],
+
+*/
+
+
+
+  // Liste des activités hors licence etc ...
+
+  var listRaw = request('GET', `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${name}&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry,place_id,icon&locationbias=point:${latitude},${longitude}&key=AIzaSyCXI24AWr0Cv2AXnbh29nVA9Ge7SPIvYBo`)
+  var response = JSON.parse(listRaw.getBody())
+
+  var placeId = response.candidates[0].place_id
+
+  //https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJN1t_tDeuEmsRUsoyG83frY4&fields=name,rating,formatted_phone_number&key=YOUR_API_KEY
+
+  var detailRaw = request('GET', `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&fields=address_component,adr_address,business_status,formatted_address,geometry,icon,name,photo,place_id,formatted_phone_number,international_phone_number,opening_hours,website,price_level,rating,review,user_ratings_total&key=AIzaSyCXI24AWr0Cv2AXnbh29nVA9Ge7SPIvYBo`)
+  var responseDetail = JSON.parse(detailRaw.getBody())
+
+
+
+  console.log(response)
+  res.json({responseDetail});
+});
 
 
 
